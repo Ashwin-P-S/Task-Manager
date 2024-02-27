026208fa6f818780775447de9fa5b36089bfab5a
Ashwin-P-S <ashwinsiva54@gmail.com>
2024-02-27 14:29:14 +0530

    backend

---
+++ backend/server.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect("mongodb://localhost:27017").then(console.log("MONGODB"));
+
+const taskSchema = new mongoose.Schema({
+    title: String,
+    description: String,
+    completed: Boolean,
+});
+
+const Task = mongoose.model("Task", taskSchema);
+
+app.get("/api/tasks", async (req, res) => {
+    try {
+        const tasks = await Task.find();
+        res.json(tasks);
+    } catch (error) {
+        console.error("Error fetching tasks:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
+app.post("/api/tasks", async (req, res) => {
+    try {
+        const { title, description } = req.body;
+        const newTask = new Task({ title, description, completed: false });
+        await newTask.save();
+        res.status(201).json(newTask);
+    } catch (error) {
+        console.error("Error creating task:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
+app.put("/api/tasks/:id", async (req, res) => {
+    try {
+        const taskId = req.params.id;
+        const { title, description, completed } = req.body;
+        const updatedTask = await Task.findByIdAndUpdate(
+            taskId,
+            { title, description, completed },
+            { new: true }
+        );
+        res.json(updatedTask);
+    } catch (error) {
+        console.error("Error updating task:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
+app.delete("/api/tasks/:id", async (req, res) => {
+    try {
+        const taskId = req.params.id;
+        await Task.findByIdAndDelete(taskId);
+        res.json({ message: "Task deleted successfully" });
+    } catch (error) {
+        console.error("Error deleting task:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
